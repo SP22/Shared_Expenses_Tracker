5aa78b93a3b74f8fc627fb803e2a92b918dd1699
SP <sp.ukr.rs@gmail.com>
2023-11-01 22:16:26 +0200

    stage 4. added Spring Boot

--- Shared Expenses Tracker/task/src/splitter/Main.java
+++ Shared Expenses Tracker/task/src/splitter/Main.java
@@ -1,9 +1,22 @@
 package splitter;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 import java.util.Scanner;
 
-public class Main {
+@SpringBootApplication
+public class Main implements CommandLineRunner {
+    private static final Logger LOG = LoggerFactory.getLogger(Main.class);
     public static void main(String[] args) {
+        SpringApplication.run(Main.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
         Scanner scanner = new Scanner(System.in);
         CommandProcessor processor = new CommandProcessor();
         String input = "";
